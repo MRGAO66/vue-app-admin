281acebeb5f648a9082f76686cecb596896d2297
LYY <gqg_1997@163.com>
2022-09-21 18:04:14 +0800

    后台管理系统初始样式

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import vuex from 'vuex'
+import router from './router'
 import App from './App.vue'
+import './plugins/ant-design-vue'
+import "@/assets/css/reset.less"
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
